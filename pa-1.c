363cef4e93b5c62e43ff34ada7121d009942039d
josephjnatale <josephjnatale@gmail.com>
2015-09-30 22:23:45 -0400

    worked on r_rotate

--- pa-1.c
+++ pa-1.c
@@ -116,7 +116,7 @@ int f_rotate(int n, int r) {
 
 	//reverse digit array
 	int switching;
-	for(int s=0; s<size/2; s++) {
+	for(int s=0; s<size/2+1; s++) {
 
 		switching=digits[size-i];
 		digits[size-1-i]=digits[i];
