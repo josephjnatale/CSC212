8ebcaa09c7f3ca267eb123316c692e505d5543ff
Joe Natale <Josephjnatale@gmail.com>
2015-10-03 14:22:57 -0400

    Improved f_sort

--- pa-1.c
+++ pa-1.c
@@ -172,7 +172,7 @@ int main() {
 			printf("\nThird number: ");
 			scanf("%i", &n3);
 
-			prinf("\n\n %i", f_sort(n1,n2,n3));
+			printf("\n\n %i", f_sort(n1,n2,n3));
 
 		}
 
@@ -188,7 +188,7 @@ int main() {
 
 int f_sort (int x, int y, int z) {
 
-	int list[3];
+	static int list[3];
 	if(x>y && x>z) {
 		list[0]=x;
 
